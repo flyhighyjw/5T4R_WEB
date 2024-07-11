2ce0813705e7635a0bf00ccbab6ef26ed7ca718c
Francisco Yoo <flyhighyjw@gmail.com>
2024-07-11 18:34:30 +0900

    로컬 구성

--- Backend/connect.php
+++ Backend/connect.php
@@ -1,7 +1,7 @@
 <?php
-$host = "43.201.33.128";
-$user = "admin";
-$pw = "1234";
+$host = "10.0.0.21";//"43.201.33.128";
+$user = "kisia";//"admin";
+$pw = "passwd";//"1234";
 $db = "Air_5T4R";
 
 $connect = new mysqli($host, $user, $pw, $db);
